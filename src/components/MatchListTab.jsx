48677511e33e25ad2d6fc1cd0f9103e7eecbcfd3
Steve Pesce <stevepesce879@gmail.com>
2020-10-01 18:08:48 -0400

    matches with portrait showing in grid on matchlisttab

--- src/components/MatchListTab.jsx
+++ src/components/MatchListTab.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {connect} from 'react-redux';
+import {Grid,Image,Icon} from 'semantic-ui-react'
 
 const MatchListTab = ({matches,currentUser}) =>
 {
@@ -23,23 +24,50 @@ const MatchListTab = ({matches,currentUser}) =>
       
       if(isActive(match))
       {
+        const {Row,Column} = Grid
+
         //if I am sender, get reciever, else get sender
         const other_user = userSentMatch(match) ? match.matched_user : match.user
-        activeMatches.push( 
-          <li key={`match-list-${other_user.email}`}>
-            {other_user.first}
-          </li> )
+        const mail = other_user.email
+        const pic = smallPic(other_user.pic);
+        
+        activeMatches.push(
+            <Row key={`match-row-${mail}`}>
+              <Column width={2} key={`match-col-0-${mail}`}>
+                <Image  className="matches-list-small-pic" 
+                        src={pic} 
+                        />
+              </Column>
+              <Column key={`match-col-1-${mail}`} width={8}>
+                <h2>{`${other_user.first} ${other_user.last}`}</h2>
+              </Column>
+              <Column key={`match-col-2-${mail}-icons`}>
+                <Icon  name={"talk"} size='big'/>
+                <Icon  name={"send"} size='big'/>
+              </Column>
+
+            </Row>
+ 
+          )
       }
     });
     return activeMatches;    
   }
 
   return(
-    <ul>
+    <Grid columns={3} >
       {mapMatchesToList()}
-    </ul>
+    </Grid>
   )
 
+}
+
+const smallPic = (addr) => {
+  return[
+    addr.slice(0,36),
+    "thumb",
+    addr.slice(36)
+  ].join("");
 }
 
 const mapStateToProps = state =>
